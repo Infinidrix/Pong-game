4e3a47680b860ad55d7e5d1cfffdb88aa87b240c
Infinidrix <biruksolomon11@gmail.com>
2021-02-07 13:23:26 +0300

    include reflect api

--- Breakout/assets/js/app.js
+++ Breakout/assets/js/app.js
@@ -1,7 +1,7 @@
 import { Ball } from "./ball.js"
 import { Enemy } from "./enemy.js"
 import { Player } from "./player.js"
-import { addScore } from "./db.js";
+import { addScore, mostFrequentPlayers } from "./db.js";
 /*
     TODO:
         Move stuff to modules and import them es6 style
@@ -33,7 +33,8 @@ document.querySelector("#btn-reset").addEventListener("click", ()=>{
 })
 
 function startGameState(){
-    ball = new Ball(canvas.clientWidth/2, canvas.clientHeight - 20, -2.2, 2.2, 10);
+    ball = Reflect.construct(Ball, [Reflect.get(canvas, "clientWidth")/2, canvas.clientHeight - 20, -2.2, 2.2, 10])
+    // ball = new Ball(canvas.clientWidth/2, canvas.clientHeight - 20, -2.2, 2.2, 10);
     playerPaddle = new Player(75, 10, 3, 0, (canvas.clientHeight-75) / 2)
     
     enemyPaddle = new Enemy(75, 10, canvas.clientWidth - 10, (canvas.clientHeight-75) / 2);
@@ -43,8 +44,8 @@ function startGameState(){
         score: 0,
         upPressed: false,
         downPressed: false,
-        canvas
     };
+    Reflect.set(gameState, 'canvas', canvas);
 
 }
 let ball, playerPaddle, enemyPaddle, gameState, interval;
@@ -78,7 +79,7 @@ function keyUpHandler(e) {
 function drawInit(){
     ctx.fillStyle = "white";
     ctx.font = "50px roboto";
-    ctx.fillText("Pong", canvas.clientWidth / 2 - 25, canvas.clientHeight / 2 - 25)
+    Reflect.apply(Reflect.getPrototypeOf(ctx).fillText, ctx, ["Pong", canvas.clientWidth / 2 - 25, canvas.clientHeight / 2 - 25])
 }
 
 function drawScore(){
@@ -110,7 +111,8 @@ function draw() {
             alert("GAME OVER");
             let playerName = document.querySelector("#input-name").value;
             console.log(`Player is ${playerName}`)
-            let leaderboards = addScore(playerName || "player", gameState.score)(showScores);
+            addScore(playerName || "player", gameState.score)((res) => showScores("#scorelist", res));
+            mostFrequentPlayers((res) => showScores("#playerlist", res))
         }, 5)
     );
     playerPaddle.move(canvas, gameState.upPressed, gameState.downPressed);
@@ -120,30 +122,34 @@ function draw() {
     drawScore();
 }
 
-export function showScores(scores){
-    let leaderboard = document.querySelector("#scorelist")
+export function showScores(nodeName, scores){
+    let leaderboard = document.querySelector(nodeName)
     leaderboard.innerHTML = ""
     let iter = returnScores(scores);
     let score = iter.next();
     while (!score.done) {
         let scoreEntry = document.createElement("li");
         let nameElem = document.createElement("mark")
-        nameElem.textContent = score.value[1];
+        nameElem.textContent = score.value[0];
         scoreEntry.appendChild(nameElem);
         let scoreElem = document.createElement("small")
-        scoreElem.textContent = score.value[2];
+        scoreElem.textContent = score.value[1];
         scoreEntry.appendChild(scoreElem);
         leaderboard.appendChild(scoreEntry);
-        score = iter.next() 
+        score = iter.next();
     } 
 }
 
 function* returnScores(scores){
     for(let i = 0; i < scores.length; i++){
-        yield formatScores`${i}|${scores[i].username}|${scores[i].score}`;
+        if (!Array.isArray(scores[i])){
+            yield formatScores`${i}|${scores[i].username}|${scores[i].score}`;
+        }
+        else{ 
+            yield formatScores`${i}|${scores[i][0]}|${scores[i][1]}`;
+        }
     }
 }
-
 function formatScores(initString, rank, username, score){
-    return [rank, username, score]
+    return [username, score]
 }
