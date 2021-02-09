cd066316d2fb25680598a2cf8d375ec50b057672
Infinidrix <biruksolomon11@gmail.com>
2021-02-09 09:23:39 +0300

    add help tooltip

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,13 @@ import Enemy from "./enemy.js"
 import Player from "./player.js"
 import { addScore, mostFrequentPlayers } from "./db.js";
 
+// initalize popovers
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
+
 // play button 
 document.querySelector("#btn-play").addEventListener("click", ()=>{
     if (!interval){
